2c79c38fe91bf315d73e5912dce636052044668b
Francois BOSMA <bosma_f@epita.fr>
2016-06-25 13:07:08 +0200

    [POLY] starting

---
+++ src/polynomial.cc
@@ -0,0 +1,5 @@
+//
+// Created by franzzy on 25/06/16.
+//
+
+#include "polynomial.hh"
---
+++ src/polynomial.hh
@@ -0,0 +1,16 @@
+//
+// Created by franzzy on 25/06/16.
+//
+
+#pragma once
+
+#include "label.hh"
+#include "weight.hh"
+
+class polynomial {
+
+public:
+private:
+    
+
+};
