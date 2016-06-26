0b8d36945a68f8e702de0f6a4b529fac92faf345
Alexis YANG <yang_e@epita.fr>
2016-06-26 03:10:58 +0100

    weight - inline

--- src/weight.hxx
+++ src/weight.hxx
@@ -19,42 +19,49 @@ Weight<T>::~Weight()
 }
 
 template <typename T>
+inline
 T Weight<T>::operator+(const Weight<T>& weight) const
 {
 	return this->label_ + weight.label_;
 }
 
 template <>
+inline
 bool Weight<bool>::operator+(const Weight<bool>& weight) const
 {
 	return this->label_ || weight.label_;
 }
 
 template <typename T>
+inline
 T Weight<T>::operator*(const Weight<T>& weight) const
 {
 	return this->label_ * weight.label_;
 }
 
 template <>
+inline
 bool Weight<bool>::operator*(const Weight<bool>& weight) const
 {
 	return this->label_ && weight.label_;
 }
 
 template <typename T>
+inline
 bool Weight<T>::operator==(const Weight<T>& weight) const
 {
 	return this->label_ == weight.label_;
 }
 
 template <typename T>
+inline
 bool Weight<T>::operator<(const Weight<T>& weight) const
 {
 	return this->label_ < weight.label_;
 }
 
 template <typename T>
+inline
 void Weight<T>::operator<<(const Weight<T>& weight) const
 {
 	std::cout << weight.label_;
