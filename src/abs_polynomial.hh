04dbace8a30a814144d8c4d3f54cc1ae5ec9b5a2
Alexis YANG <yang_e@epita.fr>
2016-06-28 13:40:14 +0100

    abs_poly - removed const for getter because we need to modify it

--- src/abs_polynomial.hh
+++ src/abs_polynomial.hh
@@ -8,7 +8,7 @@ template <typename L, typename W, template<class...> class Container, class... A
 class base_polynomial {
 	public :
 		virtual ~base_polynomial() {}
-		virtual const Container<Args...>& get_monomials() const = 0;
+		virtual Container<Args...>& get_monomials() = 0;
 		virtual bool add_monomial(const Label<L>& label, const Weight<W>& weight) = 0;
 		virtual void print(std::ostream&) const = 0;
 };
